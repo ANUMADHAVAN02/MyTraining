da3789b915b65953cc1b91b1d49a6d471ecde2d0
ANUMADHAVAN02 <146543040+ANUMADHAVAN02@users.noreply.github.com>
2023-09-30 17:25:06 +0530

    uploading VariableTypes to github repository

---
+++ VariablesTypes.java
@@ -0,0 +1,15 @@
+class VariablesTypes
+{
+	static int b=10;//STATIC
+	char c='D';//INSTANCE
+
+  public static void main (String args[])
+  {
+   long a=10000l;//LOCAL VARIABLE
+   System.out.println(a);
+   System.out.println(b);
+   System.out.println(c);
+  }
+  
+
+}
